c67d621a53cf8d8bdd2b6696c2e4b30a7867562c
kenny67nju <kenny67nju@163.com>
2021-03-04 12:17:07 +0800

    Initial commit

---
+++ src/components/demo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>{{ $store.state.count.count }}</p>
+    <div>
+      <button @click="$store.commit('increment')">+</button>
+      <button @click="$store.commit('decrement')">-</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'demo'
+}
+</script>
+
+<style scoped>
+
+</style>
