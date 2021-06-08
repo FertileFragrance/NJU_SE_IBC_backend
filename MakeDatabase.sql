8686fcf16835cfe343d27b344ea4458dc97a409f
mycprotein <ytmbx@126.com>
2021-06-08 21:15:16 +0800

    feat: update MakeDatabase.sql

--- MakeDatabase.sql
+++ MakeDatabase.sql
@@ -22,3 +22,16 @@ INSERT INTO `miniapp`.`news` (`id`, `title`, `cover`, `author`, `pubdate`
 	, `content`)
 VALUES (3, 'NJUGRID赴川大实验纪实（附实验内容及结论）', 'https://mmbiz.qpic.cn/mmbiz_jpg/FsXWsrh8wxMibq45IpXxuWz6ibdgtGXIbiaC9sEWTdibJOyibHt34JKLv4XicBv6oUGcWTbfVjL038mp1anMcy9NSCyA/640?wx_fmt=jpeg&tp=webp&wxfrom=5&wx_lazy=1&wx_co=1', 'NJUGRID科宣组', '2021-05-25 16:03:58'
 	, 'https://mp.weixin.qq.com/s/BAczjn51Iq9Q6bhr-RMDXw');
+DROP TABLE if exists `miniapp`.`orbit`;
+CREATE TABLE `miniapp`.`orbit`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `satelliteid` varchar(255) NOT NULL,
+  `satellitename` varchar(255) NOT NULL,
+  `tleone` varchar(255) NOT NULL,
+  `tletwo` varchar(255) NOT NULL,
+  `pubdate` datetime NOT NULL,
+  `updatedate` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+);
+INSERT INTO `miniapp`.`orbit` (`id`, `tle`, `pubdate`, `updatedate`)
+VALUES (1, '43663', 'GRID01', '1 43663U 18083B   21159.15375355  .00000844  00000-0  44722-4 0  9993', '2 43663  97.4520 185.5369 0010374 188.1580 261.0704 15.18069748144507', '2021-06-08 21:02:58', '2021-06-08 21:02:58');
